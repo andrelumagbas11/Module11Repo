9688207846e3703bbf26775a4f38132cf5b41893
andrelumagbas11 <andre.lumagbas11@gmail.com>
2025-11-26 20:33:16 -0500

    Updated hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 # Andre Lumagbas
-# 11 / 26 /2024
+# 11 / 26 /2025
 # A simple Python program  to print a message 
 
+# Andre Lumagbas
+# 11 / 26/ 2025
+# added a new message
 
-print("Hello, World")
+print("Hello, World!")
+
+print("Hello , GitHub!")
